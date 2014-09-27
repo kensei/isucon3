91cbca8c595bc9a972f92526c081845aca0adc52
kensei <kitahara-k@klab.com>
2014-09-27 16:10:30 +0900

    mysql index

--- webapp/config/alter.sql
+++ webapp/config/alter.sql
@@ -1,3 +1,3 @@
-alter table memos
-  add key idx_private (is_private),
-  add key idx_user (user);
+CREATE INDEX `memos_idx_is_private_created_at` ON memos (`is_private`,`created_at`);
+CREATE INDEX `memos_idx_user_created_at` ON memos (`user`,`created_at`);
+CREATE INDEX `memos_idx_user_is_private_created_at` ON memos (`user`,`is_private`,`created_at`);
